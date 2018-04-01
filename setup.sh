29ef34ad32907cfb2e703ad5d202ea787aaf4f03
ngroup <steven.nien@gmail.com>
2018-04-01 18:00:31 +0800

    update dotvim

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ DOTFILEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DOTFILEPATH/tmux/tmux.conf $HOME/.tmux.conf
 echo "Link Tmux ..."
 
+# Vim
+ln -s $DOTFILEPATH/dotvim $HOME/.vim
+echo "Link Vim ..."
